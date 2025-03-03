31b913ccef8b1cc87f50c4a6f4261a5b5864ae46
shwetgaur <146635341+shwetgaur@users.noreply.github.com>
2025-03-03 23:28:06 +0530

    Cards File

---
+++ Cards.java
@@ -0,0 +1,24 @@
+package assignment4;
+
+public class Cards {
+    private String suit;
+    private String rank;
+
+    public Cards(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
