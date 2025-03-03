4bd12543f03b3fca0c574d312bea9f847a86771e
shwetgaur <146635341+shwetgaur@users.noreply.github.com>
2025-03-03 23:29:01 +0530

    Main File

---
+++ Main.java
@@ -0,0 +1,33 @@
+package assignment4;
+
+public class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+
+
+        System.out.println("****Creating Deck****");
+
+/*  
+        System.out.println("Displaying deck");
+        deck.displayDeck();
+
+
+
+        System.out.println("shuffling deck");
+        deck.shuffleDeck();
+        deck.displayDeck();
+
+
+        deck.selectTwoRandomCards();
+
+        deck.displayCardsFromSameSuit("Spades");
+        
+
+
+
+    }
+     
+    */
+    }
+}
+
